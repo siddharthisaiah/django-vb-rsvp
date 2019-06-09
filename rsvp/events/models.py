842f263aef8f6208d01eb4b4fa5d68c7337b7408
Siddharth <siddharthisaiah@gmail.com>
2019-06-10 01:06:24 +0530

    event models and makemigrations

--- rsvp/events/models.py
+++ rsvp/events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, null=True)
+    event_datetime = models.DateField()
+    max_attendees = models.IntegerField()
+
+
+class Comment(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    posted_at = models.DateField(auto_now_add=True)
+
+
+class Attendee(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+    
