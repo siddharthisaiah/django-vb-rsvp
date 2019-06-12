c7d4d8923a472f2817bf48789c09c92ab413ff3c
Siddharth <siddharthisaiah@gmail.com>
2019-06-13 01:25:32 +0530

    show events and details from db

--- rsvp/events/views.py
+++ rsvp/events/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Event
+
+from datetime import datetime
+from .models import Event, Attendee
 # Create your views here.
 
 @login_required
 def index(request):
-    events = Event.objects.all()
-    return render(request, 'events/index.html', {events:events})
+    now = datetime.date
+    events = Event.objects.all() #Event.objects.filter(event_datetime__gte=datetime.now())
+    print(events)
+    return render(request, 'events/index.html', {"events":events})
 
 
 @login_required
 def event(request, pk):
-    context = {'title': 'RSVP - Event Details'}
+    event_details = get_object_or_404(Event, id=pk)
+    attendees = Attendee.objects.filter(event=event_details)
+    print(attendees)
+    context = {'title': 'RSVP - Event Details', 'event_details':event_details, 'attendees': attendees}
     return render(request, 'events/event_details.html', context)
