3cfcb9b46b66aa7f5529b60da3869efbe39d3563
Siddharth <siddharthisaiah@gmail.com>
2019-06-17 23:04:41 +0530

    logout link working in navbar

--- rsvp/rsvp/urls.py
+++ rsvp/rsvp/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('events/', include('events.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView, name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('admin/', admin.site.urls),
 ]
